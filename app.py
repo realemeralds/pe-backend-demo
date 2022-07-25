42b85f6464cf7fc96145e15fbd18adf5332d9e19
realemeralds <102229990+realemeralds@users.noreply.github.com>
2022-07-26 01:45:54 +0800

    :sparkles: basic implementation done

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from distutils.command.config import config
+from turtle import st
 from flask import Flask, jsonify, request
 import gspread
 from faker import Faker
@@ -12,14 +12,16 @@ sh = gc.open("backend-demo")
 ws = sh.get_worksheet(0)
 
 # config
-id_column = 1 # column with the id values
-col_offset = 3 # no of rows before the checkboxes start
+id_column = 1  # column with the id values
+col_offset = 3  # no of rows before the checkboxes start
+
 
 @app.route("/load")
 def index():
     names_list = ws.col_values(1)
     # if (ws.acell("A100").value == None):
-    names = [[i] + [fake.unique.name(), f"5{str(int(i >= 100))}{(i//10 + 1) % 10}"] + ["FALSE"]*4 for i in range(1, 200)]
+    names = [[i] + [fake.unique.name(), f"5{str(int(i >= 100))}{(i//10 + 1) % 10}"] + [
+        "FALSE"]*4 for i in range(1, 200)]
     ws.delete_rows(3, 201)
     ws.append_rows(names)
     print("appended?")
@@ -31,28 +33,30 @@ def index():
 def get():
     return jsonify({"sheet": ws.get_all_values()})
 
+
 @app.route("/post", methods=["POST"])
 def post():
     r = request
-    rDict = r.form
+    rDict = r.json
+    print(rDict)
     index = rDict.get("id", None)
     if index:
         row = ws.find(index, in_column=1).row
-        for k, v in rDict.get("data", []).values:
-            ws.update(f"{chr(64+col_offset+k)}{row}", str(bool(v)).upper(), raw=False)
-            print(f"updated cell {chr(64+col_offset+k)}{row} with value {str(bool(v)).upper()}")
+        data = rDict.get("data", [])
+        status = []
+        for k, v in data.items():
+            ws.update(f"{chr(64+col_offset+int(k))}{row}",
+                      str(bool(v)).upper(), raw=False)
+            status.append(
+                f"updated cell {chr(64+col_offset+int(k))}{row} with value {str(bool(v)).upper()}")
+            print(
+                f"updated cell {chr(64+col_offset+int(k))}{row} with value {str(bool(v)).upper()}")
     else:
         index = rDict.get("name", None)
         if index == None:
             return jsonify({"status": "error"})
-    return jsonify({"dict": rDict, "request": r})
-    
-# @app.errorhandler()
+    return jsonify({"dict": rDict, "status": ", ".join(status)})
 
-
-# the plan: have the flask app serve a gui, where names are placed alongside the 4 buttons
-# each button withh have an index, with <index no.>_<hair>_<boolean>
-# based on this, the server will update the google sheets
 
 if __name__ == "__main__":
     app.run(debug=True)
