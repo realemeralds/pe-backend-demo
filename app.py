30eea29947c54b6802f63d52aaa527f58727e199
realemeralds <102229990+realemeralds@users.noreply.github.com>
2022-08-09 04:51:32 +0800

    pain

--- app.py
+++ app.py
@@ -18,7 +18,19 @@ scopes = [
     'https://www.googleapis.com/auth/drive'
 ]
 
-creds_dict = json.loads(json_creds)
+creds_dict = {
+    "type": os.getenv('type'),
+    "project_id": os.getenv('project_id'),
+    "private_key_id": os.getenv('private_key_id'),
+    "private_key": os.getenv('private_key'),
+    "client_email": os.getenv('client_email'),
+    "client_id": os.getenv('client_id'),
+    "auth_uri": os.getenv('auth_uri'),
+    "token_uri": os.getenv('token_uri'),
+    "auth_provider_x509_cert_url": os.getenv('auth_provider_x509_cert_url'),
+    "client_x509_cert_url": os.getenv('client_x509_cert_url'),
+}
+creds_dict["private_key"] = creds_dict["private_key"].replace("\\\\n", "\n")
 print(json_creds)
 print("-"*30)
 pprint(creds_dict)
